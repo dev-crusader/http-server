c28baf866a3698c6f75b1599fad1d67ff7f307f5
saweenthapa <57078175+saweenthapa@users.noreply.github.com>
2024-04-22 19:09:30 -0500

    added load properties

--- init/load.go
+++ init/load.go
@@ -1,16 +1,30 @@
 package init
 
 import (
+	"sync"
+
 	"github.com/magiconair/properties"
 )
 
 var (
 	appProperties = make(map[string]any)
+	onceInit      sync.Once
 )
 
 const (
 	propertyKey = "propKeys"
 )
+
+func Load(path string) {
+	onceInit.Do(func() {
+		loadproperties(path)
+	})
+}
+
+func loadproperties(path string) {
+	p := properties.MustLoadFile("app.properties", properties.UTF8)
+	appProperties[propertyKey] = p
+}
 
 func GetAll() *properties.Properties {
 	p, _ := appProperties[propertyKey].(*properties.Properties)
